51cb728afb11795279210c673a9b8041fa35ed75
Oscar Zapata <0northbridge.cpu1@gmail.com>
2020-02-02 21:58:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,7 +8,7 @@ class Dog
   
   def initialize(name)
   @name = name
-  @@all << self
+  
   end
   
   def self.all 
@@ -17,6 +17,9 @@ class Dog
   
   def self.print_all
    @@all.each {|dog| puts "#{dog.name}"}
+  end 
+  def self.save
+    @@all << self
   end 
   
   def self.clear_all
